d0dd4c7e6cf3d457ecaebd3ef4ca6dc186b74598
nima <ssu87556@gmail.com>
2025-10-11 23:47:16 +0330

    Set icon to app:)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import (
 QApplication
 )
+from PySide6.QtGui import QIcon
 import qdarkstyle
 from qdarkstyle.dark.palette import DarkPalette
 from qdarkstyle.light.palette import LightPalette
@@ -9,6 +10,7 @@ from ui import Window, CONFIG
 if __name__ == "__main__":
     
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.ico"))
     CONFIG.load()
     if CONFIG.mode == "light":
         app.setStyleSheet(qdarkstyle.load_stylesheet(palette=LightPalette()))
