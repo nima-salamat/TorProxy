ac6ce9c4aab69c1c8dabc30a82aa0478a32639f7
nima <ssu87556@gmail.com>
2025-11-08 15:26:28 +0330

    Implement extract tar.gz file in to a specified dst

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import tarfile
+import logging
+
+logger = logging.getLogger(__name__)
 
 def resource_path(relative_path):
     if getattr(sys, "_MEIPASS", False):
@@ -7,3 +11,11 @@ def resource_path(relative_path):
     else:
         base = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base, relative_path)
+
+def extract_tar_gz_file(filename, dst):
+    try:
+        tar = tarfile.open(filename, "r:gz")
+        tar.extractall(dst)
+        tar.close()
+    except Exception as e:
+        logger.error(f"Extract failed: {e}")
